e412c1716c9eec92cb392331b784924769a307d9
Sebanix <tec.sastudillo@gmail.com>
2025-11-09 13:56:10 -0300

    Mini avance día 3

--- src/main/java/com/coffeelink/apicore/repository/ProductRepository.java
+++ src/main/java/com/coffeelink/apicore/repository/ProductRepository.java
@@ -2,6 +2,21 @@ package com.coffeelink.apicore.repository;
 
 import com.coffeelink.apicore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE " +
+            "(:nombre IS NULL OR lower(p.nombre) LIKE lower(concat('%', :nombre, '%'))) AND " +
+            "(:precioMin IS NULL OR p.precio >= :precioMin) AND " +
+            "(:precioMax IS NULL OR p.precio <= :precioMax)")
+    List<Product> searchProducts(
+            @Param("nombre") String nombre,
+            @Param("precioMin") BigDecimal precioMin,
+            @Param("precioMax") BigDecimal precioMax
+    );
 }
